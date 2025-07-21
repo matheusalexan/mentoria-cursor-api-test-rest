272703f1d26ce30377beb6b647635593f8b57b16
Cleidson <cleidsonthais40@gmail.com>
2025-07-21 20:55:45 -0300

    adicionei teste para bloqueio após 3 tentativas inválidas

--- test/login.test.js
+++ test/login.test.js
@@ -22,4 +22,17 @@ describe('Login - Testes principais com fixture', () => {
       throw new Error(`Esperado 401, mas retornou ${response.status}`);
     }
   });
+
+  it('deve retornar 423 após 3 tentativas inválidas', async () =>{
+    for (let i = 0; i < 3; i++) {
+      await request(app).post('/login').send(loginData.invalidUser);
+    }
+
+    const response = await request(app).post('/login').send(loginData.invalidUser);
+
+    if (response.status !==423) {
+      throw new Error(`Esperado 423, mais retornou ${response.status}`);
+    }
+
+  })
 });
